331806b888bad5999f505db136af1f03b07c3400
StefanTruong <st159375@stud.uni-stuttgart.de>
2023-05-21 19:08:56 +0200

    add: Photo class

---
+++ src/main/java/com/tuorial/stefan/photo/hochzeit/Photo.java
@@ -0,0 +1,15 @@
+package com.tuorial.stefan.photo.hochzeit;
+
+public class Photo {
+
+    private String id;
+    private String fileName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
