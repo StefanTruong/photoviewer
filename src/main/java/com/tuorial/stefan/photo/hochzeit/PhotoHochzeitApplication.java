e9b6b92f482fd0b33fb7998677afd597393cc353
StefanTruong <st159375@stud.uni-stuttgart.de>
2023-05-21 17:00:47 +0200

    add: initial commit

---
+++ src/main/java/com/tuorial/stefan/photo/hochzeit/PhotoHochzeitApplication.java
@@ -0,0 +1,13 @@
+package com.tuorial.stefan.photo.hochzeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoHochzeitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoHochzeitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuorial/stefan/photo/hochzeit/PhotoHochzeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuorial.stefan.photo.hochzeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoHochzeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
