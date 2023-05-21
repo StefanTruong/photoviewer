94678b69a29530d859a786ffbbb4725ad90207d8
StefanTruong <st159375@stud.uni-stuttgart.de>
2023-05-21 19:20:24 +0200

    add: PathVariable for specific Photos

--- src/main/java/com/tuorial/stefan/photo/hochzeit/Photo.java
+++ src/main/java/com/tuorial/stefan/photo/hochzeit/Photo.java
@@ -5,11 +5,25 @@ public class Photo {
     private String id;
     private String fileName;
 
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
     public String getId() {
         return id;
+    }
+
+    public String getFileName() {
+        return fileName;
     }
 
     public void setId(String id) {
         this.id = id;
     }
+
+
 }
--- src/main/java/com/tuorial/stefan/photo/hochzeit/PhotozController.java
+++ src/main/java/com/tuorial/stefan/photo/hochzeit/PhotozController.java
@@ -1,12 +1,28 @@
 package com.tuorial.stefan.photo.hochzeit;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PhotozController {
+
+    private List<Photo> db = List.of(
+            new Photo("1", "photo1.jpg"));
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/photo")
+    public List<Photo> get(){
+        return db;
+    }
+
+    @GetMapping("/photo/{id}")
+    public Photo get(@PathVariable String id){
+        return Null;
     }
 }
