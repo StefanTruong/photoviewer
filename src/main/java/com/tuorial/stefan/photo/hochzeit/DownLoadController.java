c2cd6b5d3c53f2376f5dfb6f5426c2c8f9b983ea
StefanTruong <st159375@stud.uni-stuttgart.de>
2023-05-23 18:44:02 +0200

    add: download method in Downloadcontroller

--- src/main/java/com/tuorial/stefan/photo/hochzeit/DownLoadController.java
+++ src/main/java/com/tuorial/stefan/photo/hochzeit/DownLoadController.java
@@ -1,8 +1,6 @@
 package com.tuorial.stefan.photo.hochzeit;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +25,12 @@ public class DownLoadController {
 
         byte[] data = photo.getData();
         HttpHeaders headers = new HttpHeaders();
-        headers.set();
+        headers.setContentType(MediaType.valueOf(photo.getContentType()));
+        ContentDisposition build = ContentDisposition
+                .builder("attachment")
+                .filename(photo.getFileName())
+                .build();
+        headers.setContentDisposition(build);
 
         return new ResponseEntity<>(data, headers, HttpStatus.OK);
     }
